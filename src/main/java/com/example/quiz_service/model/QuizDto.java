97d33ca05eb43c2faafbb9b6600d94b3774e4d96
nitinchauhan09 <thisisnitinchauhan@gmail.com>
2025-01-10 09:50:20 +0530

    Initial commit

---
+++ src/main/java/com/example/quiz_service/model/QuizDto.java
@@ -0,0 +1,34 @@
+package com.example.quiz_service.model;
+
+import lombok.Data;
+
+@Data
+public class QuizDto {
+    String category;
+    Integer numQuestions;
+    String title;
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Integer getNumQuestions() {
+        return numQuestions;
+    }
+
+    public void setNumQuestions(Integer numQuestions) {
+        this.numQuestions = numQuestions;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
